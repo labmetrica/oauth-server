fbc8a136c5bcc9164ee1eaf80c696b1f596eae0b
artfern95 <ARTfer050609>
2019-12-05 12:59:28 +0100

    añadido git ignore

--- src/main/java/com/metrica/formacion/oauthserver/dao/rolesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.metrica.formacion.oauthserver.dao;
-
-
-import com.metrica.formacion.oauthserver.entity.roles;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface rolesRepository extends JpaRepository<roles, Integer> {
-}
